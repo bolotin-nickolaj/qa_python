a3a00933e41b6c29b003e67ea1f6bad5bfd6b63d
Nikolay Bolotin <Bolotin.nickolaj@yandex.ru>
2023-12-15 17:49:24 +0300

    sprint4 commit2

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 | #  |Test name| 
 |:---|:----------|
 | 1  |test_add_new_book_add_two_books| 
-| 2  |test_set_book_genre_set_genre_for_book| 
-| 3  |test_get_book_genre_get_genre_of_book| 
+| 2  |test_add_new_book_add_book_without_genre| 
+| 3  |test_set_book_genre_set_genre_for_book| 
 | 4  |test_get_books_with_specific_genre_get_books_by_genre| 
 | 5  |test_get_books_genre_get_list_of_books| 
 | 6  |test_get_books_for_children_list_not_exist_age_rating_books| 
--- tests.py
+++ tests.py
@@ -25,30 +25,31 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
+    def test_add_new_book_add_book_without_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Стажёры')
+        assert collector.get_book_genre('Стажёры') == ''
+
     def test_set_book_genre_set_genre_for_book(self):
         collector = BooksCollector()
         collector.add_new_book('Жук в муравейнике')
         collector.set_book_genre('Жук в муравейнике', 'Фантастика')
         assert collector.get_book_genre('Жук в муравейнике') == 'Фантастика'
 
-    def test_get_book_genre_get_genre_of_book(self):
-        collector = BooksCollector()
-        collector.add_new_book('Стажеры')
-        collector.set_book_genre('Стажеры', 'Фантастика')
-        assert collector.get_book_genre('Стажеры') == 'Фантастика'
-
     def test_get_books_with_specific_genre_get_books_by_genre(self):
         collector = BooksCollector()
-        for i in 'Жук в муравейнике', 'Волны гасят ветер', 'Хромая судьба', 'Стажеры':
+        books = ['Жук в муравейнике', 'Волны гасят ветер', 'Хромая судьба', 'Стажеры']
+        for i in books:
             collector.add_new_book(i)
             collector.set_book_genre(i,'Фантастика')
         collector.add_new_book('Мальтийский сокол')
         collector.set_book_genre('Мальтийский сокол', 'Детективы')
-        assert collector.get_books_with_specific_genre('Фантастика') == ['Жук в муравейнике', 'Волны гасят ветер', 'Хромая судьба', 'Стажеры']
+        assert collector.get_books_with_specific_genre('Фантастика') == books
 
     def test_get_books_genre_get_list_of_books(self):
         collector = BooksCollector()
-        for i in 'Хромая судьба', 'Стажеры':
+        books = ['Хромая судьба', 'Стажеры']
+        for i in books:
             collector.add_new_book(i)
             collector.set_book_genre(i,'Фантастика')
         collector.add_new_book('Мальтийский сокол')
@@ -61,19 +62,17 @@ class TestBooksCollector:
         collector.set_book_genre('Мальтийский сокол', 'Детективы')
         collector.add_new_book('Дракула')
         collector.set_book_genre('Дракула','Ужасы')
-        assert not 'Дракула' in collector.get_books_for_children() and not 'Мальтийский сокол' in collector.get_books_for_children()
+        assert 'Дракула' not in collector.get_books_for_children() and 'Мальтийский сокол' not in collector.get_books_for_children()
 
     def test_add_book_in_favorites_add_one_book_in_favorites(self):
         collector = BooksCollector()
         collector.add_new_book('Жук в муравейнике')
-        collector.set_book_genre('Жук в муравейнике', 'Фантастика')
         collector.add_book_in_favorites('Жук в муравейнике')
         assert collector.get_list_of_favorites_books() == ['Жук в муравейнике']
 
     def test_delete_book_from_favorites_delete_one_book_from_favorite(self):
         collector = BooksCollector()
         collector.add_new_book('Жук в муравейнике')
-        collector.set_book_genre('Жук в муравейнике', 'Фантастика')
         collector.add_book_in_favorites('Жук в муравейнике')
         collector.delete_book_from_favorites('Жук в муравейнике')
         assert collector.get_list_of_favorites_books() == []
@@ -81,10 +80,8 @@ class TestBooksCollector:
     def test_get_list_of_favorites_books_get_list_of_favorites(self):
         collector = BooksCollector()
         collector.add_new_book('Жук в муравейнике')
-        collector.set_book_genre('Жук в муравейнике', 'Фантастика')
         collector.add_book_in_favorites('Жук в муравейнике')
         collector.add_new_book('Стажеры')
-        collector.set_book_genre('Стажеры', 'Фантастика')
         collector.add_book_in_favorites('Стажеры')
         assert collector.get_list_of_favorites_books() == ['Жук в муравейнике', 'Стажеры']
 
